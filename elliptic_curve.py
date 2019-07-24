7e2bd60e23a31f670b590e8efee77348bfd1419a
OddExtension5 <sushil.kisu@gmail.com>
2019-07-24 14:54:21 +0530

    added elliptic_curve

---
+++ elliptic_curve.py
@@ -0,0 +1,9 @@
+#  Elliptic curves are useful because of something called point addition.
+#  Point addition is where we can do an operation on two of the points on the curve and get a third point, also on the curve.
+#  It turns out that for every elliptic curve, a line will intersect it at either one point or three points, except in a couple of special cases it intersect at two points.
+#  The two exceptions are when a line is exactly vertical and when a line is tangent to the curve.
+
+#  The class represents a points in an Elliptic Curve
+
+class Point:
+    pass
