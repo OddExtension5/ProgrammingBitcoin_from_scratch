951de620b95f23156453c699683fb7edf1acc839
OddExtension5 <sushil.kisu@gmail.com>
2019-07-24 16:39:55 +0530

    added comments for class and methods

--- elliptic_curve.py
+++ elliptic_curve.py
@@ -9,6 +9,11 @@ from fieldElement import FieldElement
 
 
 class Point:
+    '''Elliptic curves have a form like this:  y**2 = x**3 + ax + b
+           Specifically, the elliptic curve used in Bitcoin is called secp256k1 and it uses this particular equation:
+                                    ||    y**2 = x**3 + 7     ||
+           The canonical form is y**2 = x**3 + ax + b, so the curve is defined by the constants a = 0, b = 7
+    '''
 
     def __init__(self, x, y, a, b):
         self.x, self.y, self.a, self.b = x, y, a, b
@@ -29,6 +34,8 @@ class Point:
             return 'Point({],{})_{}_{}'.format(self.x, self.y, self.a, self.b)
 
     def __eq__(self, other):
+        '''Points are equal if and only if they are on the same curve and have the same coordinates.
+        '''
         return self.x == other.x and self.y == other.y and self.a == other.a and self.b == other.b
 
     def __ne__(self, other):
@@ -38,15 +45,26 @@ class Point:
         if self.a != other.a or self.b != other.b:
             raise TypeError("Points ({},{}) are not on the curve.".format(self, other))
 
+        # self.x being None means that self is the point at infinity, or the additive identity.
+        # Thus, we return other.
         if self.x is None:
             return other
 
+        # other.x being None means that other is the point at infinity, or the additive identity.
+        # Thus, we return self.
         if other.x is None:
             return self
 
+        # Case 1: self.x == other.x, self.y != other.y
+        # Result is point at infinity
         if self.x == other.x and self.y != other.y:
             return type(self)(None, None, self.a, self.b)
 
+        # Case 2: self.x ≠ other.x
+        # Formula (x3,y3)==(x1,y1)+(x2,y2)
+        # s=(y2-y1)/(x2-x1)
+        # x3=s**2-x1-x2
+        # y3=s*(x1-x3)-y1
         if self.x != other.x:
             s = (other.y - self.y) / (other.x - self.x)
             x = s ** 2 - self.x - other.x
@@ -54,6 +72,11 @@ class Point:
 
             return type(self)(x, y, self.a, self.b)
 
+        # Case 3: self == other
+        # Formula (x3,y3)=(x1,y1)+(x1,y1)
+        # s=(3*x1**2+a)/(2*y1)
+        # x3=s**2-2*x1
+        # y3=s*(x1-x3)-y1
         if self == other:
             s = (3 * self.x ** 2 + self.a) / (2 * self.y)
             x = s ** 2 - 2 * self.x
@@ -61,5 +84,7 @@ class Point:
 
             return type(self)(x, y, self.a, self.y)
 
+        # Case where the tangent line is vertical
+        # If the two points are equal and the y coordinate is 0, we return the point at infinity.
         if self == other and self.y == 0 * self.x:
             return type(self)(None, None, self.a, self.b)
